a24d0d695ea0ae0e1d1ad26641b813f1f510b05d
glovas96 <alekceygolovko@gmail.com>
2024-12-30 16:04:43 +0300

    refactor: move styles to partials and use alias imports

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
 export default defineConfig({
   plugins: [react()],
+  base: '/to-do-list/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 })
